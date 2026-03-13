a180eb47228353efdafae7c4d12531b3320c31e9
Yash Bharadwaj <yashbharadwaj@Yashs-MacBook-Pro.local>
2026-03-13 09:10:31 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FloatingNav from "./components/FloatingNav";
+import { Analytics } from '@vercel/analytics/next';
 
 
 const geistSans = Geist({
@@ -143,6 +144,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <FloatingNav />
         {children}
       </body>
